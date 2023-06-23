675e062a50e1e99ad0b35c4b7c040fe036571d61
smeier-spring <117186227+smeier-spring@users.noreply.github.com>
2023-06-23 11:44:40 -0400

    finished project

---
+++ Project/ignore.js
@@ -0,0 +1,9 @@
+let img = document.querySelector("image")
+let Death= document.querySelector(".death")
+
+let Body= document.querySelector("body")
+Body.onclick=function(){
+    Death.style.display= ""
+    img.style.position= "absolute"
+
+}
